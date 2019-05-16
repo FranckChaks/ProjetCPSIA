676631d17c2c3255fff7b61603fee692dd430604
FranckChaks <franck1891@hotmail.fr>
2019-05-16 14:38:39 +0200

    bdd, index, template, login

---
+++ template.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Projet CPSIA</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="assets/css/style.css"/>
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="assets/css/animate.min.css"/>
+<!--        <link rel="shortcut icon" href="assets/css/logo.png">-->
+
+        <script type="text/javascript" src="assets/js/jquery.min.js"></script>
+        <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+<?php echo $content;?>
