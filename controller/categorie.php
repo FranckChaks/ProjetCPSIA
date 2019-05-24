c04f9445284dacc6a6758e1eb9be4e2ef71c02b1
FranckChaks <31772682+FranckChaks@users.noreply.github.com>
2019-05-24 16:26:38 +0200

    Ajout première fonction

--- controller/categorie.php
+++ controller/categorie.php
@@ -3,15 +3,37 @@ require "model/categorieModel.php";
 
 class categorie
 {
-    use genos;
+    use Genos;
 
-    public $id_c;
+    public $id;
     public $libelle_c;
 
     public function __construct(){
-        $this->id_c = 0;
-        $this->libelle_c= "";
+        $this->id      = 0;
+        $this->libelle_c = "";
+    }
+
+    public static function getList(){
+        $c        = new categorie;
+        $req      = getCategorie();        //Voir categorieModel
+        $champs   = $c->FieldList();
+        $listeCat = $c->StructList($req, $champs);
+        return $listeCat;
+    }
+
+    public static function addCategorie(){
+        $c = new categorie;
+        $c->LoadForm();
+        $c->Add();
+        header("location:categorie");
     }
 }
+
+$c = new categorie();
+$liste_categorie = $c->getList();
+if(isset($_POST['addCategorie'])){
+    $c->addCategorie();
+}
+
 
 require "view/categorieView.php";
