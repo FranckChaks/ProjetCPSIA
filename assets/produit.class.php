eb84628a419f730b74c00a9a6542cc7f229148f4
jo6tem <jonathan@net6tem.com>
2019-05-25 11:29:44 +0200

    pull

---
+++ assets/produit.class.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jonathan
+ * Date: 25/05/2019
+ * Time: 11:10
+ */
+
+class produit
+{
+    public $nom;
+    public $prix;
+
+
+
+    public function get_produit()
+    {
+
+    }
+}
---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html >
+<html lang = "fr" >
+  <head >
+    <title >Projet CPSIA POO</title >
+    <!--Required meta tags-->
+    <meta charset = "utf-8" >
+    <meta name = "viewport" content = "width=device-width, initial-scale=1, shrink-to-fit=no" >
+
+    <!--Bootstrap CSS-->
+    <link rel = "stylesheet" href = "assets/css/bootstrap.min.css">
+    <link rel = "stylesheet" href = "assets/css/animate.min.css">
+  </head >
+  <body >
+  <section class="container">
+      <div class="row">
+
+      </div>
+  </section>
+
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/css/animate.min.css"></script>
+    <script src="assets/css/bootstrap.min.css"></script>
+  </body >
+</html >
