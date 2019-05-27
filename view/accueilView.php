9fdbcc3af35afe898484a3b293aaab6d3ea77b28
FranckChaks <31772682+FranckChaks@users.noreply.github.com>
2019-05-27 16:43:21 +0200

    update accueil

--- view/accueilView.php
+++ view/accueilView.php
@@ -1,38 +1,66 @@
-<div class="container-fluid">
-    <div class="row">
-           <?php menu();?>
-            <div class="col-10">
-                <section class="front-accueil active">
-                    <div class="col-12">
-                        <h1>Clients</h1>
-                        <hr>
-<!--                        <div class="col-8">-->
-                            <table class="table table-bordered table-striped">
-                                <thead>
-                                    <tr>
-                                        <th>Nom</th>
-                                        <th>Prenom</th>
-                                        <th>Email</th>
-                                        <th>Panier</th>
-                                        <th>Action</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                <?php $i = 0; while($i < 10) {?> <!--- Test d'affichage --->
-                                    <tr>
-                                        <td>Chaks</td>
-                                        <td>Franck</td>
-                                        <td>chaks.franck@gmail.com</td>
-                                        <td><a href="#">Voir</a></td>
-                                        <td><span class="badge badge-pill badge-info">Modifier</span>  <span class="badge badge-pill badge-danger">Supprimer</span></td>
-                                    </tr>
-                                <?php $i++; } ?>
-                                </tbody>
-                            </table>
-<!--                        </div>-->
-                    </div>
-                </section>
-            </div>
-    </div>
-</div>
+<?php menu();?>
+<div class="content">
+        <section class="float-right mt-2 mr-3"> <b>0,00€</b> <i class="fa fa-shopping-cart"></i> </section>
 
+        <section class="front-bloc-accueil">
+            <h1 class="title-accueil">Les meilleurs produits<br>
+                <div  class="under-title-accueil" style="">au meilleur prix</div>
+            </h1>
+        </section>
+
+        <div class="container">
+
+            <section class="row" style="padding: 0;">
+                <div class="col-8" style="padding: 0;">
+                    <div class="front-second-accueil-left">
+                        <h2 class="txt-shdw">Retrouvez des centaines de produits à un prix imbattable.</h2>
+
+                        <div class="easy-shopping txt-shdw">
+                            <h4 class="mt-5">Sur ordinateur, tablette ou smartphone, </h4>
+                            <p class="front-accroche">faire ses courses n'a jamais été aussi simple.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4" style="padding: 0; text-align: center">
+                    <a href="produit" class="href"><div class="start-shopping">
+                        <i class="fa fa-shopping-basket"></i>
+                    </div></a>
+                    <button class="btn-produit-accueil mt-2">Voir les produits</button>
+                </div>
+            </section>
+
+            <section class="row" style="padding: 0;">
+                <div class="col-12" style="padding: 0">
+                        <h3 class="mt-3">Meilleures ventes</h3>
+                </div>
+                <div class="col-md-4 col-sm-6">
+                    <div class="bloc-product">
+                        <img src="assets/css/menager.png" height="100%" width="100%">
+                    </div>
+                    <div class="detail-product">
+                        <b>Electroménager</b>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6">
+                    <div class="bloc-product">
+                        <img src="assets/css/food.png" height="80%" width="100%">
+                    </div>
+                    <div class="detail-product">
+                        <b>Alimentaires</b>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-6">
+                    <div class="bloc-product">
+                        <img src="assets/css/jeans.png" height="100%" width="100%">
+                    </div>
+                    <div class="detail-product">
+                        <b>Vêtements</b>
+                    </div>
+                </div>
+                <div class="col-12">
+                    <a class="href" href="categorie"><button class="float-right mt-4 btn-accueil">Voir toutes les catégories <i class="fa fa-arrow-right"></i> </button></a>
+                </div>
+            </section>
+
+        </div>
+</div>
