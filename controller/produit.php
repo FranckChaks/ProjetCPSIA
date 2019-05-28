249d0edb56032f70c5c13ef43ea75c1c530276cd
FranckChaks <31772682+FranckChaks@users.noreply.github.com>
2019-05-28 14:43:15 +0200

    update controller produit

--- controller/produit.php
+++ controller/produit.php
@@ -1,14 +1,52 @@
 <?php
 
+require "model/produitModel.php";
+include('categorie.php');
 
 class produit
 {
-//    use genos;
+    use Genos;
+    public $primary_key;
+    public $id;
+    public $libelle_p;
+    public $description_p;
+    public $prix_p;
+    public $img_p;
+    public $taille_p;
+    public $id_c;
 
     public function __construct()
     {
-
+        $this ->primary_key = "id_p" ;
+        $this->id = 0;
+        $this->libelle_p = "";
+        $this->description_p = "";
+        $this->prix_p = 0;
+        $this->img_p = "";
+        $this->taille_p = null;
+        $this->id_c = 1;
     }
+
+    public static function getAllProduct(){
+        $p            = new produit();
+        $req          = getProducts();
+        $champs       = $p->FieldList();
+        $listeProduit = $p->StructList($req, $champs);
+        return $listeProduit;
+    }
+
+    public function getProductByCategory($id_c){
+        $p            = new produit();
+        $req          = getProductByCat($id_c); // ref Model
+        $champs       = $p->FieldList();
+        $listeProduit = $p->StructList($req, $champs);
+        return $listeProduit;
+    }
+}
+
+$p = new produit();
+foreach(categorie::getList() as $k=>$v){
+    $listeProduitCat[] = $p->getProductByCategory($v['id_c']);
 }
 
 require "view/produitView.php";
