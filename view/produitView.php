6ff8e7643205f930ec46ec8595a22b911a8e5ed4
FranckChaks <31772682+FranckChaks@users.noreply.github.com>
2019-05-28 14:41:25 +0200

    integration boucle affichage produit + categorie

--- view/produitView.php
+++ view/produitView.php
@@ -9,52 +9,56 @@
                         <li class="nav-item">
                             <a class="nav-link active" id="all-tab" data-toggle="pill" href="#all">Tous</a>
                         </li>
+                        <?php foreach(categorie::getList() as $k=>$v){  ?>
                         <li class="nav-item">
-                            <a class="nav-link" id="food-tab" data-toggle="pill" href="#food">Alimentaires</a>
+                            <a class="nav-link" id="<?=$v['libelle_c']?>-tab" data-toggle="pill" href="#<?=$v['libelle_c']?>"><?=$v['libelle_c']?></a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact">Electroménager</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact">High-Tech</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact">...</a>
-                        </li>
+                        <?php }?>
                     </ul>
                 </section>
             </div>
         </div>
+
         <div class="tab-content" id="pills-tabContent">
-            <div class="tab-pane fade show active" id="all">
+
+            <div class="tab-pane fade show active" id="all"> <!--Tous les produits -->
                 <div class="row mb-5">
-                <?php $i = 0; while($i <10) { ?>
+                <?php foreach(produit::getAllProduct() as $k=>$v) { ?>
                     <div class="col-lg-3 col-md-4 col-sm-6 mt-5">
                         <a href="ficheProduit" class="href">
                             <div class=" card-1 select-produit">
-                                <img src="assets/css/jeans.png" height="80%" width="100%">
-                                <p style=" padding-bottom: 10px"><b>Jeans</b><br>
-                                    <small>30€</small>
+                                <img src="assets/css/<?=$v['img_p'];?>" height="80%" width="100%">
+                                <p style=" padding-bottom: 10px">
+                                    <b><?=$v['libelle_p'];?></b>
+                                    <br>
+                                    <small><?=$v['prix_p'];?>€</small>
                                 </p>
                             </div>
                         </a>
                     </div>
-                <?php $i++; } ?>
+                <?php } ?>
                 </div>
             </div>
-            <div class="tab-pane fade" id="food">
-                    <div class="row mb-5">
-                        <?php $i = 0; while($i <10) { ?>
-                            <div class="col-lg-3 col-md-4 col-sm-6 mt-5">
-                                <div class="select-produit card-1">
-                                    <img src="assets/css/food.png" height="80%" width="100%">
-                                    <p style=" padding-bottom: 10px"><b>Légumes</b><br>
-                                        <small>10€</small></p>
-                                </div>
+
+            <?php foreach(categorie::getList() as $k=>$v){  ?>
+            <div class="tab-pane fade" id="<?=$v['libelle_c'];?>">  <!--produits par catégorie -->
+                <div class="row mb-5">
+                    <?php foreach($listeProduitCat[$v['id_c']-1] as $key=>$val){  ?>
+                        <div class="col-lg-3 col-md-4 col-sm-6 mt-5">
+                            <div class="select-produit card-1">
+                                <img src="assets/css/<?=$val['img_p'];?>" height="80%" width="100%">
+                                <p style=" padding-bottom: 10px">
+                                    <b><?=$val['libelle_p'];?></b>
+                                    <br>
+                                    <small><?=$val['prix_p'];?>€</small>
+                                </p>
                             </div>
-                        <?php $i++; } ?>
+                        </div>
+                    <?php } ?>
                 </div>
             </div>
+            <?php } ?>
         </div>
+
     </div>
 </div>
