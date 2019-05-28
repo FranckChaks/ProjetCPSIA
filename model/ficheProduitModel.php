ec992d7943820b28577a0baf6d0f424043d9f256
FranckChaks <31772682+FranckChaks@users.noreply.github.com>
2019-05-28 16:46:28 +0200

    add requete ficheProduitModel

--- model/ficheProduitModel.php
+++ model/ficheProduitModel.php
@@ -1,1 +1,10 @@
-<?php ?>
+<?php
+
+    function getFicheProduit($id_p){
+        global $bdd;
+        $req = $bdd->prepare("SELECT * FROM produit WHERE id_p = ".$id_p);
+        $req->bindValue('id_p', $id_p, PDO::PARAM_INT);
+        $req->execute();
+        return $req->fetch();
+    }
+?>
