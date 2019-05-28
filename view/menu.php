798f4e5b89c0d10d9fe18607fd0446b291f96af9
FranckChaks <31772682+FranckChaks@users.noreply.github.com>
2019-05-28 12:19:25 +0200

    changement ordre onglet

--- view/menu.php
+++ view/menu.php
@@ -3,9 +3,9 @@
         <div class="sidebar">
             <a href="accueil"><b>Logo</b></a>
             <a class="active mnu-onglet" href="accueil">Accueil</a>
+            <a href="produit" class="mnu-onglet">Produit</a>
             <a href="user" class="mnu-onglet">Gestion utilisateur</a>
             <a href="categorie" class="mnu-onglet">Gestion catégorie</a>
-            <a href="produit" class="mnu-onglet">Produit</a>
         </div>
 
     <?php }
