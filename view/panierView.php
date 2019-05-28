e56e5644cfaf35505f5bd9db1c21bf411912f676
FranckChaks <31772682+FranckChaks@users.noreply.github.com>
2019-05-28 12:19:50 +0200

    update design panier

--- view/panierView.php
+++ view/panierView.php
@@ -1,27 +1,48 @@
+<?php menu();?>
+<div class="content">
+    <div class="container">
+    <div class="row">
+        <div class="col-12 mt-5">
+            <h1>Votre Panier</h1>
 
-        <?php menu();?>
-        <div class="content">
-            <div class="front-produit" style="text-align: left">
-                <h1>Panier</h1>
-                <hr>
-                <div class="row">
-                    <?php $i = 0; while($i <5){ ?>
-                    <div class="col-1">
-                        <img src="<?=BASE_URL;?>/assets/css/wallpaper.jpg" height="60" width="60">
-                    </div>
-                    <div class="col-11">
-                       <h4>Ananas <small>1000€</small><i class="fa fa-trash float-right" title="Supprimer"></i> </h4>
-                        <b>Quantité: 5</b><br>
-                        <small>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, aliquam corporis cum dolorem dolorum earum, error fuga harum.</small>
-                        <hr>
-                    </div>
+            <table class="table mt-5">
+                <thead>
+                <tr>
+                    <th>Article</th>
+                    <th></th>
+                    <th>Prix</th>
+                    <th>Quantité</th>
+                    <th>Total</th>
+                    <th></th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php $i = 1; while($i < 5){ ?>
+                <tr>
+                    <td style="width: 76px">
+                        <img src="assets/css/food.png" height="75px" width="75px">
+                    </td>
+                    <td><b>Jeans</b></td>
+                    <td>30€</td>
+                    <td>2</td>
+                    <td>60€</td>
+                    <td><i class="fa fa-trash"></i> </td>
+                </tr>
+                <?php $i++; } ?>
+                <tr>
+                    <td><h3>Total</h3></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td><h3>240€</h3></td>
+                </tr>
+                </tbody>
+            </table>
 
-                    <?php $i++; } ?>
-                    <div class="col-12">
-                        <div class="float-right">
-                            <h3><b>Total: <small> 25 000€</small></h3>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <button class="btn-accueil float-right mr-5">Valider les achats <i class="fa fa-check-circle"></i></button>
+            <a href="produit" class="href"><button class="btn-accueil2 mr-5"> Continuer le shopping <i class="fa fa-shopping-basket"></i></button></a>
         </div>
+    </div>
+    </div>
+</div>
