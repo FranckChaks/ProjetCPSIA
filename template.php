2021109813fbf874712976e22f76686dc3e67ab3
FranckChaks <31772682+FranckChaks@users.noreply.github.com>
2019-05-28 16:38:42 +0200

    integration base_url pour lien absolu

--- template.php
+++ template.php
@@ -3,20 +3,22 @@
         <title>Projet CPSIA</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="assets/css/style.css"/>
-        <link rel="stylesheet" href="assets/css/bootstrap.min.css"/>
-        <link rel="stylesheet" href="assets/css/font-awesome.min.css"/>
-        <link rel="stylesheet" href="assets/css/animate.min.css"/>
-<!--        <link rel="shortcut icon" href="assets/css/logo.png">-->
+        <link rel="stylesheet" href="<?=BASE_URL;?>/assets/css/style.css"/>
+        <link rel="stylesheet" href="<?=BASE_URL;?>/assets/css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="<?=BASE_URL;?>/assets/css/font-awesome.min.css"/>
+        <link rel="stylesheet" href="<?=BASE_URL;?>/assets/css/animate.min.css"/>
 
 
     </head>
     <body>
-    <div class="wallpaper"></div>
 <?php echo $content;?>
-    <script type="text/javascript" src="assets/js/jquery.min.js"></script>
-    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="assets/js/fontawesome.min.js"></script>
+    <footer style="text-align: center; padding-top: 90px; color: white">
+        <p><b>Copyright 2019 - NomSite</b></p>
+    </footer>
+    <script type="text/javascript" src="<?=BASE_URL;?>/assets/js/jquery.min.js"></script>
+    <script type="text/javascript" src="<?=BASE_URL;?>/assets/js/app.js"></script>
+    <script type="text/javascript" src="<?=BASE_URL;?>/assets/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="<?=BASE_URL;?>/assets/js/fontawesome.min.js"></script>
     </body>
 
 </html>
