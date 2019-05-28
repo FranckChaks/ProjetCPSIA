e4afcd1a42e0264dac2e988b385f3e3cc143eda8
FranckChaks <31772682+FranckChaks@users.noreply.github.com>
2019-05-28 16:47:55 +0200

    update base_url menu

--- view/menu.php
+++ view/menu.php
@@ -2,10 +2,10 @@
     function menu(){ ?>
         <div class="sidebar">
             <a href="accueil"><b>Logo</b></a>
-            <a class="active mnu-onglet" href="accueil">Accueil</a>
-            <a href="produit" class="mnu-onglet">Produit</a>
-            <a href="user" class="mnu-onglet">Gestion utilisateur</a>
-            <a href="categorie" class="mnu-onglet">Gestion catégorie</a>
+            <a class="active mnu-onglet" href="<?=BASE_URL;?>/accueil">Accueil</a>
+            <a href="<?=BASE_URL;?>/produit" class="mnu-onglet">Produit</a>
+            <a href="<?=BASE_URL;?>/user" class="mnu-onglet">Gestion utilisateur</a>
+            <a href="<?=BASE_URL;?>/categorie" class="mnu-onglet">Gestion catégorie</a>
         </div>
 
     <?php }
