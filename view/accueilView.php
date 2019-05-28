40c8cfbad5f7e0ab2dec3f9d21d262f3274970c8
FranckChaks <31772682+FranckChaks@users.noreply.github.com>
2019-05-28 12:18:22 +0200

    update accueil

--- view/accueilView.php
+++ view/accueilView.php
@@ -1,66 +1,63 @@
 <?php menu();?>
 <div class="content">
-        <section class="float-right mt-2 mr-3"> <b>0,00€</b> <i class="fa fa-shopping-cart"></i> </section>
+    <section class="float-right mt-2 mr-3"><span class="mr-5">Bienvenue Admin</span> <b>0,00€</b> <i class="fa fa-shopping-cart"></i> </section>
 
-        <section class="front-bloc-accueil">
-            <h1 class="title-accueil">Les meilleurs produits<br>
-                <div  class="under-title-accueil" style="">au meilleur prix</div>
-            </h1>
-        </section>
+    <section class="front-bloc-accueil">
+        <h1 class="title-accueil">Les meilleurs produits<br>
+            <div  class="under-title-accueil" style="">au meilleur prix</div>
+        </h1>
+    </section>
 
-        <div class="container">
+    <section class="row row-nopadding">
+        <div class="col-8" style="padding: 0;">
+            <div class="front-second-accueil-left">
+                <h2 class="txt-shdw">Retrouvez des centaines d'articles à un prix imbattable.</h2>
 
-            <section class="row" style="padding: 0;">
-                <div class="col-8" style="padding: 0;">
-                    <div class="front-second-accueil-left">
-                        <h2 class="txt-shdw">Retrouvez des centaines de produits à un prix imbattable.</h2>
-
-                        <div class="easy-shopping txt-shdw">
-                            <h4 class="mt-5">Sur ordinateur, tablette ou smartphone, </h4>
-                            <p class="front-accroche">faire ses courses n'a jamais été aussi simple.</p>
-                        </div>
-                    </div>
+                <div class="easy-shopping txt-shdw">
+                    <h4 class="mt-5">Sur ordinateur, tablette ou smartphone, </h4>
+                    <p class="front-accroche">faire ses courses n'a jamais été aussi simple.</p>
                 </div>
-                <div class="col-4" style="padding: 0; text-align: center">
-                    <a href="produit" class="href"><div class="start-shopping">
-                        <i class="fa fa-shopping-basket"></i>
-                    </div></a>
-                    <button class="btn-produit-accueil mt-2">Voir les produits</button>
-                </div>
-            </section>
-
-            <section class="row" style="padding: 0;">
-                <div class="col-12" style="padding: 0">
-                        <h3 class="mt-3">Meilleures ventes</h3>
-                </div>
-                <div class="col-md-4 col-sm-6">
-                    <div class="bloc-product">
-                        <img src="assets/css/menager.png" height="100%" width="100%">
-                    </div>
-                    <div class="detail-product">
-                        <b>Electroménager</b>
-                    </div>
-                </div>
-                <div class="col-md-4 col-sm-6">
-                    <div class="bloc-product">
-                        <img src="assets/css/food.png" height="80%" width="100%">
-                    </div>
-                    <div class="detail-product">
-                        <b>Alimentaires</b>
-                    </div>
-                </div>
-                <div class="col-md-4 col-sm-6">
-                    <div class="bloc-product">
-                        <img src="assets/css/jeans.png" height="100%" width="100%">
-                    </div>
-                    <div class="detail-product">
-                        <b>Vêtements</b>
-                    </div>
-                </div>
-                <div class="col-12">
-                    <a class="href" href="categorie"><button class="float-right mt-4 btn-accueil">Voir toutes les catégories <i class="fa fa-arrow-right"></i> </button></a>
-                </div>
-            </section>
-
+            </div>
         </div>
+        <div class="col-4" style="padding: 0; text-align: center">
+            <a href="produit" class="href"><div class="start-shopping">
+                <i class="fa fa-shopping-basket"></i>
+            </div></a>
+            <a href="produit" class="href"><button class="btn-produit-accueil mt-2">Voir les produits</button></a>
+        </div>
+    </section>
+
+    <section class="row row-nopadding">
+        <div class="col-12" style="padding: 0">
+                <h3 class="mt-3">Meilleures ventes</h3>
+        </div>
+        <div class="col-md-4 col-sm-6">
+            <div class="bloc-product">
+                <img src="assets/css/menager.png" height="100%" width="100%">
+            </div>
+            <div class="detail-product">
+                <b>Electroménager</b>
+            </div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+            <div class="bloc-product">
+                <img src="assets/css/food.png" height="80%" width="100%">
+            </div>
+            <div class="detail-product">
+                <b>Alimentaires</b>
+            </div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+            <div class="bloc-product">
+                <img src="assets/css/jeans.png" height="100%" width="100%">
+            </div>
+            <div class="detail-product">
+                <b>Vêtements</b>
+            </div>
+        </div>
+        <div class="col-12">
+            <a class="href" href="categorie"><button class="float-right mt-4 btn-accueil">Voir toutes les catégories <i class="fa fa-arrow-right"></i> </button></a>
+        </div>
+    </section>
+
 </div>
