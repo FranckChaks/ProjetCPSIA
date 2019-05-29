b7667a83500de3bd5f7f09ecd13689ee654c9482
FranckChaks <31772682+FranckChaks@users.noreply.github.com>
2019-05-29 15:03:57 +0200

    update ficheProduitView

--- view/panierView.php
+++ view/panierView.php
@@ -1,48 +1,77 @@
 <?php menu();?>
 <div class="content">
+    <section class="float-right mt-2 mr-3"><span class="mr-5">Bienvenue Admin</span> <a href="<?=BASE_URL;?>/panier" class="href"><b>0,00€</b> <i class="fa fa-shopping-cart"></i></a>  </section><br>
     <div class="container">
-    <div class="row">
-        <div class="col-12 mt-5">
-            <h1>Votre Panier</h1>
+        <div class="row" style="width: 100%">
+            <div class="col-6 mt-5">
+                <img src="<?=BASE_URL;?>/assets/css/<?=$img;?>" height="400px" width="80%">
+            </div>
+            <div class="col-6 mt-5" style="border-left: 1px solid rgba(182,182,182,0.72)">
+                <?=$modif;?>
+                <h1><?=$libelle;?></h1>
+                <h3><?=$prix;?>€</h3>
 
-            <table class="table mt-5">
-                <thead>
-                <tr>
-                    <th>Article</th>
-                    <th></th>
-                    <th>Prix</th>
-                    <th>Quantité</th>
-                    <th>Total</th>
-                    <th></th>
-                </tr>
-                </thead>
-                <tbody>
-                <?php $i = 1; while($i < 5){ ?>
-                <tr>
-                    <td style="width: 76px">
-                        <img src="assets/css/food.png" height="75px" width="75px">
-                    </td>
-                    <td><b>Jeans</b></td>
-                    <td>30€</td>
-                    <td>2</td>
-                    <td>60€</td>
-                    <td><i class="fa fa-trash"></i> </td>
-                </tr>
-                <?php $i++; } ?>
-                <tr>
-                    <td><h3>Total</h3></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td><h3>240€</h3></td>
-                </tr>
-                </tbody>
-            </table>
+                <form action="#" method="post">
+                    <h4 class="mt-5">
+                        <small>//Si c'est des fringues--></small>
+                         Taille
+                        <small>
+                            <select name="size" class="ml-4">
+                                <option value="xs">XS</option>
+                                <option value="s">S</option>
+                                <option value="m">M</option>
+                                <option value="l">L</option>
+                                <option value="xl">XL</option>
+                                <option value="xxl">XXL</option>
+                            </select>
+                        </small>
+                    </h4>
 
-            <button class="btn-accueil float-right mr-5">Valider les achats <i class="fa fa-check-circle"></i></button>
-            <a href="produit" class="href"><button class="btn-accueil2 mr-5"> Continuer le shopping <i class="fa fa-shopping-basket"></i></button></a>
+                    <h4 class="mt-3">Quantité
+                        <small>
+                            <select name="quantite" class="ml-1">
+                            <?php $i = 1; while($i <= 15){ ?>
+                                <option value="<?=$i;?>"><?=$i;?></option>
+                                <?php $i++; } ?>
+                            </select>
+                        </small>
+                    </h4>
+
+                    <button type="submit" name="addPanier" class="btn-accueil mt-5 mb-5"><i class="fa fa-shopping-basket"></i> Ajouter au panier</button>
+                </form>
+                <h4>Description</h4>
+                <p><?=$desc;?></p>
+            </div>
+        </div>
+
+        <div class="row mt-5">
+            <div class="col-12">
+                <h4>Voir aussi</h4>
+                <hr>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <a href="ficheProduit" class="href"><div class="card-1 select-produit">
+                        <img src="<?=BASE_URL;?>/assets/css/jeans.png" height="80%" width="70%">
+                        <p style=" padding-bottom: 10px"><b>Jeans</b><br>
+                            <small>30€</small></p>
+                    </div></a>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <a href="ficheProduit" class="href"><div class="card-1 select-produit">
+                        <img src="<?=BASE_URL;?>/assets/css/jeans.png" height="80%" width="70%">
+                        <p style=" padding-bottom: 10px"><b>Jeans</b><br>
+                            <small>30€</small></p>
+                    </div></a>
+            </div>
+            <div class="col-md-4 col-sm-6">
+                <a href="ficheProduit" class="href"><div class="card-1 select-produit">
+                        <img src="<?=BASE_URL;?>/assets/css/jeans.png" height="80%" width="70%">
+                        <p style=" padding-bottom: 10px"><b>Jeans</b><br>
+                            <small>30€</small></p>
+                    </div></a>
+            </div>
         </div>
     </div>
-    </div>
+
+
 </div>
