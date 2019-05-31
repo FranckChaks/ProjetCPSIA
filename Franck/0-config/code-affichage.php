c2558bf3bfe2f37a07f50c1f49482866f31a8979
FranckChaks <31772682+FranckChaks@users.noreply.github.com>
2019-05-31 16:55:15 +0200

    update

--- Franck/0-config/code-affichage.php
+++ Franck/0-config/code-affichage.php
@@ -25,5 +25,6 @@ function js(){
     <script src="<?=URL_HOME?>/js/jquery.min.js"> </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="<?=URL_HOME?>/js/bootstrap.min.js"> </script>
+    <script src="<?=URL_HOME?>/js/app.js"> </script>
     <?php
 }
